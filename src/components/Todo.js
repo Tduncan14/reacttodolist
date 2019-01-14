949b1b3603cdd41754d3936d224e9aa017b97b12
unknown <nbatre1@yahoo.com>
2019-01-14 09:29:41 -0600

    you will become a software engineer

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,21 @@
 import React,{Component} from 'react';
-
-
+import TodoItem from './Todoitem';
+import PropTypes from 'prop-types';
 class Todos extends Component{
    
     render(){
          console.log(this.props.todos);
         return  this.props.todos.map((todo) =>(
-            <h3>{todo.title}</h3>
+            <TodoItem key={todo.id} todo={todo} />
         ));
 
 
         }
     }
+
+    //PropTypes
+     Todos.protoType = {
+         todos:PropTypes.array.isRequired
+     }
 
     export default Todos;
