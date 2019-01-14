19ff260a195d29ff2cc59045ec27cbf0127dc6fd
unknown <nbatre1@yahoo.com>
2019-01-14 13:24:41 -0600

    keep moving forward with coding

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -36,7 +36,7 @@ export class TodoItem extends Component {
             <div style={this.getStyle()}>
            <p><input type="checkbox" onChange={this.props.markComplete.bind(this,id)}/>{' '}
            {title}
-           <button style={btnStyle}>x</button></p>
+           <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button></p>
            
          
             </div>
@@ -53,7 +53,7 @@ const btnStyle ={
     background:'#ff0000',
     color:'#fff',
     border: 'none',
-    padding:'5px 10px',
+    padding:'5px 9px',
     borderRadius:'50%',
     cursor:'pointer',
     float:'right'
