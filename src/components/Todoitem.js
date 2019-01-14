4899931484ff37474fdf4e522ce45d83b64fe891
unknown <nbatre1@yahoo.com>
2019-01-14 09:40:07 -0600

    you will be a great software engineer

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
 
 
 export class TodoItem extends Component {
     render(){
         return(
-            <div>
+            <div style={{backgroundColor:'#f4f4f4'}}>
            <p>{this.props.todo.title}</p>
             </div>
         )
     }
 }
+
+TodoItem.propTypes= {
+    todo:PropTypes.object.isRequired
+}
+
+// you can also use variables
+
 
 export default TodoItem
